35e1416473763e454832aa9f3852a51dd4353100
Руслан <almatov.365@mail.ru>
2023-01-14 21:41:39 +0500

    Update

--- analytic/main/views.py
+++ analytic/main/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return render(request, 'main/about.html')
+def demand(request):
+    return render(request, 'main/demand.html')
+
+def geography(request):
+    return render(request, 'main/geography.html')
+
+def skills(request):
+    return render(request, 'main/skills.html')
+
+def last_vacancies(request):
+    return render(request, 'main/last_vacancies.html')
+
